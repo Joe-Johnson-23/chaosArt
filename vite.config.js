037d8a491227fd6b1c8d1a02f0404c203d76b2ee
Joseph Johnson <josephjohnson@Josephs-MacBook-Pro.local>
2025-02-27 10:57:30 -0600

    Add Heroku domain to allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: process.env.PORT || 5173,
+    allowedHosts: [
+      'the-works-of-joe-johnson-2c33aa058b4b.herokuapp.com',
+      '.herokuapp.com'  // This allows all Heroku domains
+    ]
+  }
 })
